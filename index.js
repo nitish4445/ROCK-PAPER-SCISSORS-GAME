ede2dc8d43317b2a3b6c5d1d2477d39c55dfa057
nitish4445 <nitishkumar09598@gmail.com>
2025-04-29 00:12:09 +0530

    3rd commit

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ let computer=document.querySelectorAll(".computer");
 // console.log(computer);
 let user=document.querySelector(".user");
 let pc=document.querySelector(".pc");
-
+let win_model=document.querySelector(".win-model");
+let winner=document.querySelector(".winner");
 let option_line=document.getElementById("option-line");
 let option_before=document.getElementById("option-before");
 let option_after=document.getElementById("option-after");
+let play=document.querySelector(".play");
+let computer_score=document.getElementById("computer-score")
+let Our_score=document.getElementById("our-score")
+let score=JSON.parse(localStorage.getItem("score"))
 let random=Math.floor(Math.random()*3)
+
 // console.log(random);
 con.forEach((element,index)=>{
     element.addEventListener("click",()=>{
@@ -24,10 +30,26 @@ con.forEach((element,index)=>{
         setTimeout(()=>{
             pc.style.opacity="1";
             setTimeout(()=>{
-                con[random].style.display="block";
-                con[random].classList.add("right");
-            },1000)
+                computer[random].style.display="block";
+                computer[random].classList.add("right");
+            },1000);
+            setTimeout(()=>{
+                if(index==random){
+                    win_model.style.display="grid";
+                    winner.innerText="TIE UP";
+                    play.innerText="REPLAY"
+                }
+                else if(index==0 && random==2 || index==1 && random==0|| index==2&& random==1){
+                    win_model.style.display="grid";
+                    winner.innerText="YOU WIN";
+                }else{
+                    win_model.style.display="grid";
+                    winner.innerText="YOU LOOSE";
+                }
+            })
         },500)
     })
 })
-
+play.addEventListener("click",()=>{
+    window.location.reload();
+})
